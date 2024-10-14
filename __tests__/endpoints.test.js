805df5f18532aa49dcb1e3401f58e7b86580e4f0
iresa <iresacho@gmail.com>
2024-10-14 21:12:58 +0100

    add get all articles feature

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -75,6 +75,24 @@ describe('NC news endpoint tests',()=>{
                 })
             })
         })
-        
+        describe('GETting all articles',()=>{
+            test('/api/articles',()=>{
+                return request(app)
+                .get('/api/articles')
+                .expect(200)
+                .then(({body})=>{
+                    body.articles.forEach(article=>{
+                        expect(typeof article.article_id).toBe('number');
+                        expect(typeof article.title).toBe('string');
+                        expect(typeof article.author).toBe('string');
+                        expect(typeof article.topic).toBe('string');
+                        expect(typeof article.created_at).toBe('string');
+                        expect(typeof article.votes).toBe('number');
+                        expect(typeof article.article_img_url).toBe('string');
+                        expect(typeof article.comment_count).toBe('number');
+                    })
+                })
+            })
+        })
     })
 })
--- models/models.js
+++ models/models.js
@@ -19,6 +19,23 @@ exports.fetchArticleById = (id) => {
     })
 }
 
+exports.fetchAllArticles = ()=>{
+    return db.query(`SELECT articles.article_id, 
+        articles.title, 
+        articles.author, 
+        articles.topic, 
+        articles.created_at, 
+        articles.votes, 
+        articles.article_img_url, 
+        COUNT(comments.comment_id) AS comment_count 
+        FROM articles 
+        LEFT JOIN comments ON articles.article_id = comments.article_id 
+        GROUP BY articles.article_id 
+        ORDER BY articles.created_at DESC`)
+        .then(({rows})=>{
+            return rows
+        })
+}
 exports.fetchEndpoints= ()=>{
     return endpoints
 }
