d894105258ee9dd10d7a524513120e584bb3a56b
iresa <iresacho@gmail.com>
2024-10-14 18:04:26 +0100

    add get article by id feature and update endpoints.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllTopics, serverErrorHandler, customErrorHandler, psqlErrorHandler, allErrorHandler, getEndpoints } = require('./controllers/controllers');
+const { getAllTopics, serverErrorHandler, customErrorHandler, psqlErrorHandler, allErrorHandler, getEndpoints, getArticleById } = require('./controllers/controllers');
 const app = express();
 
 app.get('/api/topics', getAllTopics);
 
 app.get('/api', getEndpoints);
+
+app.get('/api/articles/:article_id', getArticleById);
 
 app.use(psqlErrorHandler);
 
--- models/models.js
+++ models/models.js
@@ -9,6 +9,16 @@ exports.selectAllTopics = ()=>{
     })
 }
 
+exports.fetchArticleById = (id) => {
+    return db.query(`SELECT * from articles WHERE article_id = $1`, [id])
+    .then(({rows})=>{
+        if(rows.length === 0){
+            return Promise.reject({status:404, msg: "article does not exist"})
+        }
+        return rows[0]
+    })
+}
+
 exports.fetchEndpoints= ()=>{
     return endpoints
 }
