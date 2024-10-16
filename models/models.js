62e4461b104b7ab746a87bc5e85f2753ea2ad1a3
iresa <iresacho@gmail.com>
2024-10-16 18:42:09 +0100

    add 13-get-article-by-id
    add a comment_count column to the article fetched by article id

--- models/models.js
+++ models/models.js
@@ -14,7 +14,20 @@ exports.selectAllTopics = ()=>{
 }
 
 exports.fetchArticleById = (id) => {
-    return db.query(`SELECT * from articles WHERE article_id = $1`, [id])
+    return db.query(`SELECT articles.article_id, 
+            articles.title, 
+            articles.topic, 
+            articles.author,
+            articles.body, 
+            articles.created_at, 
+            articles.votes, 
+            articles.article_img_url,
+            CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count
+        FROM articles 
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        WHERE articles.article_id = $1
+        GROUP BY articles.article_id`, [id])
+        
     .then(({rows})=>{
         if(rows.length === 0){
             return Promise.reject({status:404, msg: "article does not exist"})
